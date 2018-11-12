4d4d86bd91c17a9d9c74156718934ec7ea596bbf
Ahmed Hefnawi <me@ahmedhefnawi.com>
2018-11-12 06:25:30 +0200

    products API using Flask Rest+

--- services/products/api.py
+++ services/products/api.py
@@ -1,135 +1,55 @@
-import os
-import flask
-import tempfile
-import flask_sqlalchemy
-import flask_praetorian
-import flask_cors
-
-db = flask_sqlalchemy.SQLAlchemy()
-guard = flask_praetorian.Praetorian()
-cors = flask_cors.CORS()
+from flask import Flask, request
+from flask_restplus import Resource, Api, fields
+from json_storage_manager import atomic, utils
 
 
-# A generic user model that might be used by an app powered by flask-praetorian
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Text, unique=True)
-    password = db.Column(db.Text)
-    roles = db.Column(db.Text)
-    is_active = db.Column(db.Boolean, default=True, server_default='true')
+products_json_file = "data.json"
 
-    @property
-    def rolenames(self):
-        try:
-            return self.roles.split(',')
-        except Exception:
-            return []
+app = Flask(__name__)
 
-    @classmethod
-    def lookup(cls, username):
-        return cls.query.filter_by(username=username).one_or_none()
+api = Api(app, version='1.0', title='Products API',
+          description='REST API for managing products')
 
-    @classmethod
-    def identify(cls, id):
-        return cls.query.get(id)
-
-    @property
-    def identity(self):
-        return self.id
-
-    def is_valid(self):
-        return self.is_active
+products_ns = api.namespace('api/v1.0/products', description='Products API V1.0')
 
 
-# Initialize flask app
-app = flask.Flask(__name__)
-app.debug = os.environ['DEBUG']
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-app.config['JWT_ACCESS_LIFESPAN'] = {'hours': int(os.environ['JWT_ACCESS_LIFESPAN'])}
-app.config['JWT_REFRESH_LIFESPAN'] = {'days': int(os.environ['JWT_REFRESH_LIFESPAN'])}
+product_item = api.model('Product', {
+    'uuid': fields.String(readOnly=True, description='The unique identifier of a product'),
+    'name': fields.String(required=True, description='Product name'),
+    'price': fields.Float(required=True, description='Product price'),
+})
 
-# Initialize the flask-praetorian instance for the app
-guard.init_app(app, User)
+@products_ns.route('/')
+class Products(Resource):
+    def get(self):
+        pass
 
-# Initialize a local database [[DEMO]]
-local_database = tempfile.NamedTemporaryFile(prefix='local', suffix='.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(local_database)
-db.init_app(app)
-
-# Initializes CORS so that the api_tool can talk to the example app
-cors.init_app(app)
-
-# Add users for demo
-with app.app_context():
-    db.create_all()
-    db.session.add(User(
-        username='jeferson',
-        password=guard.encrypt_password('jef-pass-go'),
-        roles='admin'
-    ))
-    db.session.add(User(
-        username='hefnawi',
-        password=guard.encrypt_password('hef-pass-go'),
-        roles='admin'
-    ))
-    db.session.commit()
+    @api.response(201, 'Product successfully created.')
+    @api.expect(product_item)
+    def post(self):
+        """Creates a new product."""
+        if utils.is_file(products_json_file):
+            result = atomic.set_item(str(products_json_file), request.json)
+            if result:
+                return {"message": "Product successfully created."}, 201
 
 
-# Set up the routes
+@products_ns.route('/<uuid:uuid>')
+@api.response(404, 'Product not found.')
+class Products(Resource):
+    def get(self, uuid):
+        if utils.is_file(products_json_file):
+            results = atomic.get_item(str(products_json_file), str(uuid))
+            if results:
+                return results, 200
+        return {"message": "Product not found."}, 404
 
-@app.route('/login', methods=['POST'])
-def login():
-    """
-    Logs a user in by parsing a POST request containing user credentials and
-    issuing a JWT token.
+    def put(self):
+        pass
 
-    .. example::
-       $ curl http://localhost:5000/login -X POST \
-         -d '{"username":"hefnawi","password":"hef-pass-go"}'
-    """
-    req = flask.request.get_json(force=True)
-    username = req.get('username', None)
-    password = req.get('password', None)
-    user = guard.authenticate(username, password)
-    ret = {'access_token': guard.encode_jwt_token(user)}
-    return (flask.jsonify(ret), 200)
+    def delete(self):
+        pass
 
 
-@app.route('/api/v0.1/products/')
-@flask_praetorian.auth_required
-def protected():
-    """
-    A protected endpoint. The auth_required decorator will require a header
-    containing a valid JWT
-
-    .. example::
-       $ curl http://localhost:5000/protected -X GET \
-         -H "Authorization: Bearer <your_token>"
-    """
-    return flask.jsonify(message='protected endpoint (allowed user {})'.format(
-        flask_praetorian.current_user().username,
-    ))
-
-
-@app.route('/protected_admin_required')
-@flask_praetorian.roles_required('admin')
-def protected_admin_required():
-    """
-    A protected endpoint that requires a role. The roles_required decorator
-    will require that the supplied JWT includes the required roles
-
-    .. example::
-       $ curl http://localhost:5000/protected_admin_required -X GET \
-          -H "Authorization: Bearer <your_token>"
-    """
-    return flask.jsonify(
-        message='protected_admin_required endpoint (allowed user {})'.format(
-            flask_praetorian.current_user().username,
-        )
-    )
-
-
-# Run the demo
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
